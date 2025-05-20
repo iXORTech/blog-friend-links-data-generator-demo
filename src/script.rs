7e1476fa89adfda897c5c972d0df49124fd773f1
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2025-05-20 12:17:53 -0400

    chore: Add Documentation for Config Structs
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src/script.rs
+++ src/script.rs
@@ -7,12 +7,23 @@
 
 use serde::Deserialize;
 
+/// The structure of the script configuration.
+///
+/// It contains two parts:
+/// - `github`: Configuration for GitHub API access.
+/// - `generation`: Configuration for the data generation process.
 #[derive(Deserialize)]
 struct Config {
     github: GithubConfig,
     generation: GenerationConfig
 }
 
+/// The structure of the GitHub configuration.
+///
+/// It contains:
+/// - `token`: The GitHub API access token.
+/// - `owner`: The owner of the GitHub repository where issues to be processed are located.
+/// - `repository`: The name of the GitHub repository where issues to be processed are located.
 #[derive(Deserialize)]
 struct GithubConfig {
     token: String,
@@ -20,6 +31,12 @@ struct GithubConfig {
     repository: String,
 }
 
+/// The structure of the data generation configuration.
+///
+/// It contains:
+/// - `label`: The label added to the issues to be included in the generated data.
+/// - `groups`: The groups that separate issues and generated data into different categories.
+/// - `sort_by_updated_time`: Whether to sort the issues by their updated time or creation time.
 #[derive(Deserialize)]
 struct GenerationConfig {
     label: String,
