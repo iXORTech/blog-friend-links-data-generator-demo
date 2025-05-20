9d3d1d8fcdf18589785227377fd8c85e870628ba
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2025-05-19 22:56:46 -0400

    feat: Add Config File
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- src/script.rs
+++ src/script.rs
@@ -1,8 +1,42 @@
 #!/usr/bin/env rust-script
 //! ```cargo
 //! [dependencies]
+//! serde = { version = "1.0.219", features = ["derive"] }  # Serialization/Deserialization
+//! toml = "0.8.22" # TOML Parsing
 //! ```
 
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Config {
+    github: GithubConfig,
+    generation: GenerationConfig
+}
+
+#[derive(Deserialize)]
+struct GithubConfig {
+    token: String,
+    owner: String,
+    repository: String,
+}
+
+#[derive(Deserialize)]
+struct GenerationConfig {
+    label: String,
+    groups: Vec<String>,
+    sort_by_updated_time: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Read the config.toml file and parse it.
+    let config_file: String = std::fs::read_to_string("config.toml").expect("Failed to Read Configuration File");
+    let config: Config = toml::from_str(&config_file).expect("Failed to Parse Configuration");
+
+    println!("Github Token: {}", config.github.token);
+    println!("Github Owner: {}", config.github.owner);
+    println!("Github Repository: {}", config.github.repository);
+
+    println!("Generation Label: {}", config.generation.label);
+    println!("Generation Groups: {:?}", config.generation.groups);
+    println!("Sort by Updated Time: {}", config.generation.sort_by_updated_time);
 }
